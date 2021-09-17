138d21b503b9c4c4ffa4a93778010e68fb591ec0
carcastan <carla.castaneda@g.austincc.edu>
2021-09-17 14:47:53 -0500

    master

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -17,8 +17,11 @@ int main()
 	cin>>num;
 
 	int result = multiply_numbers(num);
+	cout<<"Result: "<<result<<"\n";
 
-	
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Result: "<<result<<"\n";	
 
 	return 0;
 }
